04dc498d06e95404d67b7e2fd08cc3b8f6ba63ab
Marcos Soares <mavmaso@gmail.com>
2021-02-07 01:39:40 -0300

    test:[WIP] first socket

---
+++ test/luta_web/channels/socket_test.exs
@@ -0,0 +1,23 @@
+defmodule Luta.SocketTest do
+  use LutaWeb.ChannelCase
+
+  alias LutaWeb.{UserSocket}
+
+  # setup do
+  #   ChannelSessions.clear
+
+  #   {:ok}
+  # end
+
+  describe "sockets" do
+    test "primeiro" do
+      {:ok, soc} = connect(UserSocket, %{id: "1"}) |> IO.inspect
+      {:ok, _, soc} = subscribe_and_join(soc, ChatRoom, "room:chat")
+
+      push(soc, "envia", %{msg: "Ola"})
+
+      assert_push "recebe", %{msg: message}
+      assert message.content == "Ola"
+    end
+  end
+end
