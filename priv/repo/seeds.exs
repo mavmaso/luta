e38f5055e4ecb8b1a1e5903a69e3f2b1f894c762
Marcos Soares <mavmaso@gmail.com>
2021-02-07 15:35:49 -0300

    chore: update seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,5 +1,19 @@
 alias Luta.Repo
 
+## USERS
+
+Repo.insert!(%Luta.Auth.User{
+  login: "sasuke",
+  password: "$2b$12$iWNYYuxNcQhaUuJ82jLKu..jbrQQl8..it6K5AvdVovOwDmLX2OVu",
+  nick: "sasukerk"
+})
+
+Repo.insert!(%Luta.Auth.User{
+  login: "test1",
+  password: "$2b$12$iWNYYuxNcQhaUuJ82jLKu..jbrQQl8..it6K5AvdVovOwDmLX2OVu",
+  nick: "test1"
+})
+
 ## CHARS
 
 Repo.insert!(%Luta.Char.Fighter{
@@ -8,6 +22,14 @@ Repo.insert!(%Luta.Char.Fighter{
   def: "3",
   spd: "7",
   hps: "65",
+})
+
+Repo.insert!(%Luta.Char.Fighter{
+  title: "Sword Player",
+  atk: "7",
+  def: "3",
+  spd: "6",
+  hps: "90",
 })
 
 
@@ -19,6 +41,14 @@ Repo.insert!(%Luta.Cards.MoveSet{
   power: 5,
   special: "nada",
   start_up: 3
+})
+
+Repo.insert!(%Luta.Cards.MoveSet{
+  type: "D",
+  description: "Corte Duplo",
+  power: 8,
+  special: "nada",
+  start_up: 5
 })
 
 Repo.insert!(%Luta.Cards.MoveSet{
